df0fdf2288735414e19314b0cef056021aa74ba7
FERNANDO <joferguzvas@gmail.com>
2023-03-01 02:54:27 -0600

    cambia modo oscuro

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
+
+toggleTheme.addEventListener('click', ()=>{
+    document.body.classList.toggle("dark");
+    if (toggleIcon.src.includes('moon.svg')) {
+        toggleIcon.src = 'assets/icons/sun.svg';
+        toggleText.textContent = 'Light mode';
+    }
+});
